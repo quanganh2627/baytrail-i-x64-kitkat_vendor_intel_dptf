be53eda34c5d674df898ca4ec06e42010be47fac
vphirema <vijay.p.hiremath@intel.com>
2014-05-19 22:41:00 -0700

    dptf: Conditionally compile the DPTF code
    Added INTEL_FEATURE_DPTF condition to compile the DPTF code conditionally
    in the respective target.
    
    Change-Id: I8dde251a02e324d507b5cb408561434d6701ccc5
    Signed-off-by: vphirema <vijay.p.hiremath@intel.com>
    Reviewed-on: https://android.intel.com/191944
    Reviewed-by: Chittim Munirathnam, MadhusudanX <madhusudanx.chittim.munirathnam@intel.com>
    Tested-by: Chittim Munirathnam, MadhusudanX <madhusudanx.chittim.munirathnam@intel.com>
    Reviewed-on: https://android.intel.com/279452
    Reviewed-by: Kankroliwala, Huzefa NomanX <huzefa.nomanx.kankroliwala@intel.com>
    Tested-by: Kankroliwala, Huzefa NomanX <huzefa.nomanx.kankroliwala@intel.com>

--- Android.mk
+++ Android.mk
@@ -1,4 +1,6 @@
+ifeq ($(INTEL_FEATURE_DPTF),true)
 INTEL_DPTF_ROOT_PATH:= $(call my-dir)
 include $(INTEL_DPTF_ROOT_PATH)/Products/ESIF_UF/Androidx86/jni/Android.mk
 include $(INTEL_DPTF_ROOT_PATH)/Products/DPTF/Androidx86/jni/Android.mk
 include $(INTEL_DPTF_ROOT_PATH)/Packages/Androidx86/jni/Android.mk
+endif
